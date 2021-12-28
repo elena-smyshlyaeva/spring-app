ebd3c4ca91701d7b8a7c485fe1f704603403ad61
Elena <elena.smyshlyaeva01@gmail.com>
2021-12-28 18:10:54 +0300

    added findByUsername

--- src/main/java/ru/sumbirsoft/chat/repository/UserRepository.java
+++ src/main/java/ru/sumbirsoft/chat/repository/UserRepository.java
@@ -3,5 +3,8 @@ package ru.sumbirsoft.chat.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.sumbirsoft.chat.domain.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
