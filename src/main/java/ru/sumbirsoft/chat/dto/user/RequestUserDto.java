44e6c4ff50c702c42d8470f4cf57a6463fddc902
Elena <elena.smyshlyaeva01@gmail.com>
2021-12-28 18:04:08 +0300

    deleted extra fields

--- src/main/java/ru/sumbirsoft/chat/dto/user/RequestUserDto.java
+++ src/main/java/ru/sumbirsoft/chat/dto/user/RequestUserDto.java
@@ -1,15 +1,9 @@
 package ru.sumbirsoft.chat.dto.user;
 
 import lombok.Data;
-import ru.sumbirsoft.chat.domain.Room;
-
-import java.util.Set;
 
 @Data
 public class RequestUserDto {
     private String username;
-    private String login;
     private String password;
-    Set<Room> ownerIn;
-    Set<Room> rooms;
 }
--- src/main/java/ru/sumbirsoft/chat/dto/user/ResponseUserDto.java
+++ src/main/java/ru/sumbirsoft/chat/dto/user/ResponseUserDto.java
@@ -1,15 +1,9 @@
 package ru.sumbirsoft.chat.dto.user;
 
 import lombok.Data;
-import ru.sumbirsoft.chat.domain.Members;
-import ru.sumbirsoft.chat.domain.Room;
-
-import java.util.Set;
 
 @Data
 public class ResponseUserDto {
     private long userId;
     private String username;
-    private Set<Room> ownerIn;
-    private Set<Members> userRooms;
 }
