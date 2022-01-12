6055145f85f0b952e909ccf37daadf32cd66896d
Elena <elena.smyshlyaeva01@gmail.com>
2022-01-12 15:23:26 +0300

    new permission for message and room

--- src/main/java/ru/sumbirsoft/chat/domain/Permission.java
+++ src/main/java/ru/sumbirsoft/chat/domain/Permission.java
@@ -5,7 +5,16 @@ package ru.sumbirsoft.chat.domain;
  */
 
 public enum Permission {
-    USER_READ("user:read"), USER_WRITE("user:write");
+    USER_READ("user:read"), USER_WRITE("user:write"),
+
+    USER_BAN("user:ban"), //block user
+    USER_UNBAN("user:unban"), //unblock user
+    USER_APPOINT_MODER("user:appoint_moder"), //appoint new moderator
+    USER_DELETE_MODER("user:delete_moder"), //disappoint moderator
+
+    MESSAGE_DELETE("message:delete"),
+
+    ROOM_DELETE_USER("room:delete_user");
 
     private final String permission;
 
--- src/main/java/ru/sumbirsoft/chat/domain/Role.java
+++ src/main/java/ru/sumbirsoft/chat/domain/Role.java
@@ -5,8 +5,24 @@ import java.util.Set;
 import java.util.stream.Collectors;
 
 public enum Role {
-    USER(Set.of(Permission.USER_READ)),
-    ADMIN(Set.of(Permission.USER_READ, Permission.USER_WRITE));
+    USER(Set.of(
+            Permission.USER_READ
+    )),
+    MODERATOR(Set.of(
+            Permission.USER_READ,
+            Permission.USER_BAN,
+            Permission.USER_UNBAN,
+            Permission.MESSAGE_DELETE
+    )),
+    ADMIN(Set.of(
+            Permission.USER_READ,
+            Permission.USER_WRITE,
+            Permission.USER_BAN,
+            Permission.USER_UNBAN,
+            Permission.USER_APPOINT_MODER,
+            Permission.USER_DELETE_MODER,
+            Permission.MESSAGE_DELETE
+    ));
 
     private final Set<Permission> permissions;
 
