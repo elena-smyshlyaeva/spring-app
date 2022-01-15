1bce02f6b4fd5ca5cf1bba695cd70a71c3c41488
Elena <elena.smyshlyaeva01@gmail.com>
2022-01-15 17:35:23 +0300

    replace Members to MembersDto

--- src/main/java/ru/sumbirsoft/chat/mapper/RoomMapper.java
+++ src/main/java/ru/sumbirsoft/chat/mapper/RoomMapper.java
@@ -1,11 +1,12 @@
 package ru.sumbirsoft.chat.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import ru.sumbirsoft.chat.domain.Room;
 import ru.sumbirsoft.chat.dto.room.RequestRoomDto;
 import ru.sumbirsoft.chat.dto.room.ResponseRoomDto;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {MembersMapper.class})
 public interface RoomMapper {
     Room responseRoomDtoToRoom(ResponseRoomDto responseRoomDto);
     ResponseRoomDto roomToResponseRoomDto(Room room);
